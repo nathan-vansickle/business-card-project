ab3b71da328ed06b21edea4b8f116da883685652
nathan-vansickle <ndvans03@gmail.com>
2022-10-08 15:23:56 -0400

    Added user authentication scheme

--- js/login.js
+++ js/login.js
@@ -7,12 +7,34 @@ const loginButton = document.getElementById('login-button');
 const emailField = document.getElementById('email-field');
 const passwordField = document.getElementById('password-field');
 
+const signupButton = document.getElementById('signup-button');
+const signupEmailField = document.getElementById('signup-email-field');
+const signupPasswordField = document.getElementById('signup-password-field');
+
 function checkCreds() {
     const email = emailField.value;
-    const password = passwordField.valuel;
+    const password = passwordField.value;
 
-    console.log(email);
-    console.log(password);
+    if (users.has(email) && users.get(email) == password) {
+        alert('correct');
+        // Go to user page
+    } else {
+        alert('Invalid email or password');
+    }
+}
+
+function addUser() {
+    const email = signupEmailField.value;
+    const password = signupPasswordField.value;
+
+    if (users.has(email)) {
+        alert('Email already in use. Please enter a different email address.');
+    } else {
+        users.set(email, password);
+        // Go to card creation wizard
+    }
+    
 }
 
 loginButton.addEventListener('click', checkCreds);
+signupButton.addEventListener('click', addUser)
