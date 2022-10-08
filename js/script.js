5a9a4af974dbbeee8be6feed94b17511cb4c795f
nathan-vansickle <ndvans03@gmail.com>
2022-10-08 13:44:50 -0400

    Did a bunch of stuff

--- js/script.js
+++ js/script.js
@@ -1,11 +1,33 @@
 "use strict";
 
+let i = 1;
+
+//---ELEMENTS---
 const button = document.getElementById('btn');
+const numberField = document.getElementById("number");
+const businessCards = document.querySelector('.business-cards');
 
 
-function getCardID() {
-    const cardId = document.getElementById("number").value;
-    console.log(cardId);
+function updateCardList() {
+    const cardId = numberField.value;
+    addCard(cardId);
 }
 
-button.addEventListener('click', getCardID);
+function addCard(number) {
+    const html = generateHTML();
+
+    businessCards.innerHTML += html;
+}
+
+function generateHTML() {
+    const html = `
+    <div class=\"card\">
+        <div class=\"card-data\">
+            <h1 id=\"name\">Name</h1>
+        </div>
+    </div>`
+
+    return html;
+}
+
+button.addEventListener('click', updateCardList);
